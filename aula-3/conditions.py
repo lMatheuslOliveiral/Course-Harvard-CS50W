47987f2e06965d750c9225e4d3b2aa8c44377223
Matheus Oliveira <matheus-oliveira-batista@hotmail.com>
2023-09-19 20:58:15 +0100

    Conditions very good

---
+++ aula-3/conditions.py
@@ -0,0 +1,8 @@
+n = int(input("Numer: "))
+
+if n > 0:
+    print(f"{n} is positive!")
+elif n < 0:
+    print(f"{n} is negative!")
+else:
+    print(" is zero!")
--- aula-3/test.py
+++ aula-3/test.py
@@ -1,2 +1,5 @@
-print ("Hello, World!")
- idade = 18
+name = input("Name: ")
+print("Hello! " + name)
+
+idade = input("Idade: ")
+print(f"Ok! Sua idade e: {idade}")
