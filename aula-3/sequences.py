1e40cd7fe77b862705a545bc49bf2f2fc4d989a5
Matheus Oliveira <matheus-oliveira-batista@hotmail.com>
2023-09-19 22:24:32 +0100

    Aula muito boa!

---
+++ aula-3/sequences.py
@@ -0,0 +1,11 @@
+names = ["Caio", "Julia", "Yan"]
+print(names)
+
+print(names[0])
+
+names.append("Grau")
+
+print(names)
+
+test = (10, 11)
+print(test)
---
+++ aula-3/set.py
@@ -0,0 +1,9 @@
+conjunto = set()
+conjunto.add(1)
+conjunto.add(3)
+conjunto.add(5)
+conjunto.add(5)
+conjunto.add(1)
+print(conjunto)
+
+print(f"A quantidade e: {len(conjunto)} de elementos!")
